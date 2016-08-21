35e6b99733668b136231a7ffe27474cbfd4af4d0
Lance Zukel <lzukel@gmail.com>
2016-08-20 22:08:57 -0500

    Create add_disk_vm_machine.py

---
+++ add_disk_vm_machine.py
@@ -0,0 +1,18 @@
+ dev_changes = []
+        new_disk_kb = int(disk_size) * 1024 * 1024
+        disk_spec = vim.vm.device.VirtualDeviceSpec()
+        disk_spec.fileOperation = "create"
+        disk_spec.operation = vim.vm.device.VirtualDeviceSpec.Operation.add
+        disk_spec.device = vim.vm.device.VirtualDisk()
+        disk_spec.device.backing = \
+            vim.vm.device.VirtualDisk.FlatVer2BackingInfo()
+        if disk_type == 'thin':
+            disk_spec.device.backing.thinProvisioned = True
+        disk_spec.device.backing.diskMode = 'persistent'
+        disk_spec.device.unitNumber = unit_number
+        disk_spec.device.capacityInKB = new_disk_kb
+        disk_spec.device.controllerKey = controller.key
+        dev_changes.append(disk_spec)
+        spec.deviceChange = dev_changes
+        vm.ReconfigVM_Task(spec=spec)
+        print "%sGB disk added to %s" % (disk_size, vm.config.name)
